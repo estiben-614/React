49534b2823fedfe671dd5c3c32f0860c67b10dfc
Estiben Fernandez Echeverri <efernandeze@unal.edu.co>
2024-05-02 22:42:22 -0500

    Guardar en localStorage

--- src/08 - useReducer/TodoApp.jsx
+++ src/08 - useReducer/TodoApp.jsx
@@ -1,26 +1,11 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { todoReducer } from "./todoReducer";
 import TodoList from "./TodoList";
 import TodoAdd from "./TodoAdd";
 
-const initialValues = [
-  {
-    id: new Date().getTime(),
-    description: "Holardas",
-    done: false,
-  },
-  {
-    id: new Date().getTime(),
-    description: "Holardas x2",
-    done: false,
-  },
-  {
-    id: new Date().getTime(),
-    description: "Holardas x3",
-    done: false,
-  },
-];
+
 const TodoApp = () => {
+  const initialValues = JSON.parse(localStorage.getItem('todo')) || [];
   const [todos, dispatch] = useReducer(todoReducer, initialValues);
 
   const onHandleTodoForm = (event, value) => {
@@ -35,9 +20,12 @@ const TodoApp = () => {
       type: 'ADD',
       payload: newTodo
     }
-    console.log(action)
     dispatch(action)
   };
+
+  useEffect(() => {
+    localStorage.setItem('todo', JSON.stringify(todos))
+  }, [todos])
   return (
     <>
       <h1>Todo App</h1>
